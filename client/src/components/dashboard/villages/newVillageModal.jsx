4e97558715ed620e6270ec620ab2655adc569042
Sampath Bandla <112122835+sampath99999@users.noreply.github.com>
2023-03-14 08:38:43 +0530

    Updated Villages Page

---
+++ client/src/components/dashboard/villages/newVillageModal.jsx
@@ -0,0 +1,79 @@
+import React, { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+
+function NewVillageModal(props) {
+    const { isOpen, closeModal } = props;
+    return (
+        <>
+            <Transition appear show={isOpen} as={Fragment}>
+                <Dialog
+                    as="div"
+                    className="relative z-10"
+                    open={isOpen}
+                    onClose={closeModal}
+                >
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-black bg-opacity-25" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 overflow-y-auto">
+                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                    <Dialog.Title
+                                        as="h3"
+                                        className="text-lg font-medium leading-6 text-gray-900"
+                                    >
+                                        New Village
+                                    </Dialog.Title>
+                                    <div className="mt-2">
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label
+                                                htmlFor="first-name"
+                                                className="block text-sm font-medium leading-6 text-gray-900"
+                                            >
+                                                Name
+                                            </label>
+                                            <input
+                                                type="text"
+                                                className="block outline-none w-full rounded-md border-0 py-1.5 pl-4 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                placeholder="New Village Name"
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="mt-4">
+                                        <button
+                                            type="button"
+                                            className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                        >
+                                            Create
+                                        </button>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition>
+        </>
+    );
+}
+
+export default NewVillageModal;
--- client/src/pages/dashboard/villages.jsx
+++ client/src/pages/dashboard/villages.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import BreadCrumb from "../../components/dashboard/breadCrumb";
-
+import { BiPlus } from "react-icons/bi";
+import { Dialog } from "@headlessui/react";
+import NewVillageModal from "../../components/dashboard/villages/newVillageModal";
 function Villages() {
+    let [isAddModalOpen, setIsAddModalOpen] = useState(false);
+
+    function closeModal() {
+        setIsAddModalOpen(false);
+    }
+    function openModal() {
+        setIsAddModalOpen(true);
+    }
     let breadCrumb = [
         {
             name: "Dashboard",
@@ -19,7 +29,16 @@ function Villages() {
     return (
         <div>
             <BreadCrumb items={breadCrumb} />
-            <h1 className="font-semibold text-2xl mt-3">Villages</h1>
+            <div className="flex items-center justify-between mb-2">
+                <h1 className="font-semibold text-2xl mt-3">Villages</h1>
+                <button
+                    className="bg-indigo-700 text-white px-4 py-2 rounded-lg flex items-center gap-2"
+                    onClick={openModal}
+                >
+                    <BiPlus />
+                    New Village
+                </button>
+            </div>
             <hr />
 
             <p className="text-sm text-gray-500 mt-1 w-3/4 text-justify">
@@ -29,6 +48,13 @@ function Villages() {
                 management tools, you can optimize network performance, improve
                 customer satisfaction, and streamline operations.
             </p>
+
+            {/* Modal */}
+            <NewVillageModal
+                isOpen={isAddModalOpen}
+                closeModal={closeModal}
+                openModal={openModal}
+            ></NewVillageModal>
         </div>
     );
 }
