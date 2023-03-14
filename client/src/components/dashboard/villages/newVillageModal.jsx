32540961a4b0d6bf9bd8809e3d060f6a2b45b2a9
sampath <sampathbandla@lewisu.edu>
2023-03-14 18:07:19 +0530

    updated village

--- client/src/components/dashboard/villages/newVillageModal.jsx
+++ client/src/components/dashboard/villages/newVillageModal.jsx
@@ -1,79 +1,30 @@
 import React, { Fragment, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-
-function NewVillageModal(props) {
-    const { isOpen, closeModal } = props;
-    return (
-        <>
-            <Transition appear show={isOpen} as={Fragment}>
-                <Dialog
-                    as="div"
-                    className="relative z-10"
-                    open={isOpen}
-                    onClose={closeModal}
-                >
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-black bg-opacity-25" />
-                    </Transition.Child>
-
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex min-h-full items-center justify-center p-4 text-center">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
-                            >
-                                <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                                    <Dialog.Title
-                                        as="h3"
-                                        className="text-lg font-medium leading-6 text-gray-900"
-                                    >
-                                        New Village
-                                    </Dialog.Title>
-                                    <div className="mt-2">
-                                        <div className="col-span-6 sm:col-span-3">
-                                            <label
-                                                htmlFor="first-name"
-                                                className="block text-sm font-medium leading-6 text-gray-900"
-                                            >
-                                                Name
-                                            </label>
-                                            <input
-                                                type="text"
-                                                className="block outline-none w-full rounded-md border-0 py-1.5 pl-4 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                                placeholder="New Village Name"
-                                            />
-                                        </div>
-                                    </div>
-
-                                    <div className="mt-4">
-                                        <button
-                                            type="button"
-                                            className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                                        >
-                                            Create
-                                        </button>
-                                    </div>
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
-                    </div>
-                </Dialog>
-            </Transition>
-        </>
-    );
+import { AiOutlineClose } from "react-icons/ai";
+function NewVillage(props) {
+	return (
+		<>
+			<tr className="">
+				<td className="p-2">#</td>
+				<td className="p-2">
+					<input
+						type="text"
+						placeholder="Village Name"
+						className="relative px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6 outline-none"
+					/>
+				</td>
+				<td className="p-2">
+					<button className="items-center justify-center rounded-md border border-transparent bg-indigo-600 text-white hover:bg-indigo-700 px-3 py-1">
+						Save
+					</button>
+				</td>
+				<td>
+					<button>
+						<AiOutlineClose />
+					</button>
+				</td>
+			</tr>
+		</>
+	);
 }
 
-export default NewVillageModal;
+export default NewVillage;
--- client/src/pages/dashboard/villages.jsx
+++ client/src/pages/dashboard/villages.jsx
@@ -1,62 +1,60 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { createRef, useState } from "react";
 import BreadCrumb from "../../components/dashboard/breadCrumb";
 import { BiPlus } from "react-icons/bi";
-import { Dialog } from "@headlessui/react";
-import NewVillageModal from "../../components/dashboard/villages/newVillageModal";
+import NewVillage from "../../components/dashboard/villages/newVillageModal";
 function Villages() {
-    let [isAddModalOpen, setIsAddModalOpen] = useState(false);
+	let breadCrumb = [
+		{
+			name: "Dashboard",
+			link: "/dashboard",
+		},
+		{
+			name: "Assets",
+			link: "../assets",
+		},
+		{
+			name: "Villages",
+		},
+	];
+	let tableBody = createRef();
+	let handleNewVillage = function () {
+		tableBody.current.appendChild(<NewVillage />);
+	};
+	return (
+		<div>
+			<BreadCrumb items={breadCrumb} />
+			<div className="flex items-center justify-between mb-2">
+				<h1 className="font-semibold text-2xl mt-3">Villages</h1>
+				<button
+					onClick={handleNewVillage}
+					className="bg-indigo-700 text-white px-4 py-2 rounded-lg flex items-center gap-2"
+				>
+					<BiPlus />
+					New Village
+				</button>
+			</div>
+			<hr />
 
-    function closeModal() {
-        setIsAddModalOpen(false);
-    }
-    function openModal() {
-        setIsAddModalOpen(true);
-    }
-    let breadCrumb = [
-        {
-            name: "Dashboard",
-            link: "/dashboard",
-        },
-        {
-            name: "Assets",
-            link: "../assets",
-        },
-        {
-            name: "Villages",
-        },
-    ];
-    return (
-        <div>
-            <BreadCrumb items={breadCrumb} />
-            <div className="flex items-center justify-between mb-2">
-                <h1 className="font-semibold text-2xl mt-3">Villages</h1>
-                <button
-                    className="bg-indigo-700 text-white px-4 py-2 rounded-lg flex items-center gap-2"
-                    onClick={openModal}
-                >
-                    <BiPlus />
-                    New Village
-                </button>
-            </div>
-            <hr />
+			<p className="text-sm text-gray-500 mt-1 w-3/4 text-justify">
+				In the Assets section of our dashboard, you can manage your
+				cable network's critical components. This includes managing
+				users, villages, streets, and service packages. With our asset
+				management tools, you can optimize network performance, improve
+				customer satisfaction, and streamline operations.
+			</p>
 
-            <p className="text-sm text-gray-500 mt-1 w-3/4 text-justify">
-                In the Assets section of our dashboard, you can manage your
-                cable network's critical components. This includes managing
-                users, villages, streets, and service packages. With our asset
-                management tools, you can optimize network performance, improve
-                customer satisfaction, and streamline operations.
-            </p>
-
-            {/* Modal */}
-            <NewVillageModal
-                isOpen={isAddModalOpen}
-                closeModal={closeModal}
-                openModal={openModal}
-            ></NewVillageModal>
-        </div>
-    );
+			<table className="table table-auto w-full max-w-3xl mt-5 text-sm text-left text-gray-500 dark:text-gray-500">
+				<thead className="text-xs text-gray-800 uppercase bg-gray-100">
+					<tr>
+						<th className="font-medium w-1/6 p-2">S.No</th>
+						<th className="font-medium w-4/6 p-2">Name</th>
+						<th className="font-medium w-1/6 p-2">Action</th>
+					</tr>
+				</thead>
+				<tbody ref={tableBody}></tbody>
+			</table>
+		</div>
+	);
 }
 
 export default Villages;
